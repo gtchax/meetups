b10a91f502d8ff5264b33c67a31c0d6abf9f9512
Gwinyai Chakonda <gtchax@gmail.com>
2017-10-03 20:41:42 +0800

    Init react and loopback

---
+++ client_src/src/components/Meetups.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Meetups extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+render(){
+    return (
+      <div>
+        <h1>Meetups</h1>
+      </div>
+    );
+  }
+
+}
+
+export default Meetups;
